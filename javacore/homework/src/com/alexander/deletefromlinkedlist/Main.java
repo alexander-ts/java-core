dc66e47268b0d0f070c460e07fe87e5168ce921e
Alexander <alexander.tsukanovvv@gmail.com>
2016-10-09 19:09:28 +0300

    Added additional homework

---
+++ javacore/homework/src/com/alexander/deletefromlinkedlist/Main.java
@@ -0,0 +1,54 @@
+package com.alexander.deletefromlinkedlist;
+
+import java.util.*;
+
+
+
+
+
+
+public class Main {
+
+
+    public static void removemid ( LinkedList<Integer> list){
+
+            int size = list.size() / 2;
+            list.remove(size);
+
+
+
+    }
+
+
+    public static void main(String[] args) {
+
+        LinkedList<Integer> list = new LinkedList<Integer>();
+
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+
+
+        removemid(list);
+        removemid(list);
+
+
+        System.out.println(list.toString());
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
