87f5ebb86dbeb4e419045726ce2bf528c8827ea8
Alexander <alexander.tsukanovvv@gmail.com>
2016-10-10 09:00:13 +0300

    Added additional homework

---
+++ javacore/homework/src/com/alexander/doginterface/Bark.java
@@ -0,0 +1,11 @@
+package com.alexander.doginterface;
+
+/**
+ * Created by alex on 1/31/16.
+ */
+public interface Bark
+{
+
+
+    public void Barking();
+}
---
+++ javacore/homework/src/com/alexander/doginterface/Main.java
@@ -0,0 +1,9 @@
+package com.alexander.doginterface;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+    }
+}
