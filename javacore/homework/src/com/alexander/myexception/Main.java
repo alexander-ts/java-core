d8f4dc67fb3badecdefe5ec115678ac3c4e4f536
Alexander <alexander.tsukanovvv@gmail.com>
2016-10-11 17:06:18 +0300

    Added additional homework

---
+++ javacore/homework/src/com/alexander/myexception/Main.java
@@ -0,0 +1,29 @@
+package com.alexander.myexception;
+
+class StringSetter{
+    StringSetter(String s) throws ContainsLetterException{
+        if (s.contains("s")) {
+            throw  new ContainsLetterException("Error!");
+        }
+        else{
+            System.out.println("Success!");
+        }
+    }
+}
+
+class ContainsLetterException extends Error{
+   ContainsLetterException(String s){
+     super(s);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) throws ContainsLetterException {
+        try{
+            StringSetter a = new StringSetter("s");
+        }
+        catch (ContainsLetterException ex){
+            System.out.println("Please, put another letter");
+        }
+    }
+}
